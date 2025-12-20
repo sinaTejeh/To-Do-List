e2012275435b2e4a0228256bd87b6d811f4702b2
sinaTejeh <amrjv1378@gmail.com>
2025-12-20 18:17:48 +0330

    some changes about rendering li functionality :add a input checkbox instead of changing image

--- script.js
+++ script.js
@@ -7,52 +7,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //////////////////////////////////////
     const render = function (data) {
-        const html = `  <li class="li">
-                        <button class="check">
-                        <img src="/img/1.png" class="image">
-                        </button>
-                      ${data}
-                        <button class="close">&times</button>
-                    </li>`;
-        listContainer.insertAdjacentHTML('afterend', html);
+        return `    <input type="checkbox" class="checkbox">
+                    <span>${data}</span>
+                     <button class="close">&times</button>
+                     <button class="edit">|||</button>
+                    `;
     };
 
-    let x = 0;
-    const checkTask = function (btn, img) {
-        if (x == 0) {
-            img.src = '/img/3.png';
-            x = 1;
-        } else {
-            img.src = '/img/1.png';
-            x = 0;
-        }
-        btn.parentElement.classList.toggle('checked');
-    }
-
-    addBtn.addEventListener('click', function () {
+    const addTask = function () {
         const data = input.value.trim();
-        console.log(data);
-        if (!input.value) {
+        if (!data) {
             alert('you have to write s.th');
             input.value = '';
             return;
         };
-        render(data);
-        //clear input box
+        const li = document.createElement('li');
+        li.textContent = data;
+        li.innerHTML = render(data)
+        listContainer.appendChild(li);
         input.value = '';
+    };
 
-
-        const checkBtn = document.querySelector('.check');
-        const image = document.querySelector('.image');
-        checkBtn.addEventListener('click', function () {
-            checkTask(checkBtn, image);
-        });
-
-        const closeBtn = document.querySelector('.close');
-        closeBtn.addEventListener('click', function () {
-            closeBtn.parentElement.remove();
-        });
-    });
-
-})
+    addBtn.addEventListener('click', addTask);
+});
 
